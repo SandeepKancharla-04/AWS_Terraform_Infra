ee2d6fb5dbb114b84a9308e2c77c9e25504a46ac
SandeepKancharla-04 <sunnysandeep0707@gmail.com>
2025-08-26 12:12:15 -0400

    ci: offline terraform plan (mock creds, skip validations, CI env); remove aws_ami data source

--- providers.tf
+++ providers.tf
@@ -5,6 +5,7 @@ provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 
+  # Make plan work without real AWS: skip validations and metadata/account calls
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_region_validation      = true
