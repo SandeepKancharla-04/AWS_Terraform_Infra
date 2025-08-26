0425110490dc0df89358e1f5cecedecb028fff5e
SandeepKancharla-04 <sunnysandeep0707@gmail.com>
2025-08-26 11:31:33 -0400

    fix: declare ami_id default in compute module and remove AWS AMI data source

--- modules/compute/variables.tf
+++ modules/compute/variables.tf
@@ -2,4 +2,7 @@ variable "project_name" { type = string }
 variable "vpc_id" { type = string }
 variable "private_subnets" { type = list(string) }
 variable "instance_type" { type = string }
-variable "ami_id" { type = string }
+variable "ami_id" {
+  type    = string
+  default = "ami-1234567890abcdef0"
+}
