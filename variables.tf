3dca05a6ceb8c709fd960dc0a63e647cd597e3b5
SandeepKancharla-04 <sunnysandeep0707@gmail.com>
2025-08-26 11:34:48 -0400

    fix: add default for root ami_id

--- variables.tf
+++ variables.tf
@@ -31,4 +31,5 @@ variable "instance_type" {
 variable "ami_id" {
   description = "AMI ID for the app instance (e.g., ami-xxxxxxxxxxxxxxx)"
   type        = string
+  default     = "ami-1234567890abcdef0"
 }
