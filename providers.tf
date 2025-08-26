f1285d2180f378a0eaaedba0ea34051fd79b1ed7
SandeepKancharla-04 <sunnysandeep0707@gmail.com>
2025-08-26 11:38:13 -0400

    ci: allow plan without AWS creds (skip validation + dummy env)

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = var.aws_region
+
+  # Allow terraform plan to run in CI without real AWS creds
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_region_validation      = true
+  skip_requesting_account_id  = true
 }
